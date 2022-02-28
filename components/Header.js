ca86ad2fd56c7927f8ead4db79581b98ff3ae7b1
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-02-28 18:35:36 +0530

    added instagram logo

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+      <div>
+        <div className='flex justify-between max-w-6xl'>
+            <div className="relative hidden w-24 h-24 lg:inline-grid">
+            <Image src="https://links.papareact.com/ocw" layout='fill' objectFit="contain"/>
+              </div>
+              <div className="relative  w-10 h-10 lg:hidden flex-shrink-0">
+            <Image src="https://links.papareact.com/jjm" layout='fill' objectFit="contain"/>
+              </div>
+        </div>  
+      </div>
+    
   )
 }
 
